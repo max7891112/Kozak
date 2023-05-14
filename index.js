2feec5b900dc7d4fc8562529a528a74218762ccb
max7891112 <max.alkhimovich@gmail.com>
2023-05-14 14:12:40 +1000

    actual version 14.05

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-new Swiper('.image-slider', {
-
-    navigation: {
-        nextEl: '.swiper-button-next-modern',
-        prevEl: '.swiper-button-prev-modern'
-    },
-
-    pagination: {
-        el: '.swiper-paginations',
-        //буллеты
-        clickable: true
-    },
-});
 import Form from "./components/form/Form.js";
-import ModalForm from "./components/modal/modalForm/ModalForm.js";
 import Questions from "./components/questions/Questions.js";
 import SecondForm from "./components/secondForm/SecondForm.js";
 import TechnicalFunctions from "./technicalFunctions/TechnicalFunctions.js";
@@ -23,5 +9,20 @@ document.addEventListener('DOMContentLoaded', () => {
     TechnicalFunctions.addListenerDataClose();
     TechnicalFunctions.addListenerForModalForm();
     TechnicalFunctions.addListenerForModalCase();
+    TechnicalFunctions.smoothNavigation();
+    new Swiper('.image-slider', {
+
+        navigation: {
+            nextEl: '.swiper-button-next-modern',
+            prevEl: '.swiper-button-prev-modern'
+        },
+    
+        pagination: {
+            el: '.swiper-paginations',
+            //буллеты
+            clickable: true
+        },
+        loop: true,
+    });
 })
 
--- technicalFunctions/TechnicalFunctions.js
+++ technicalFunctions/TechnicalFunctions.js
@@ -20,37 +20,39 @@ class TechnicalFunctions {
         for(let tel of inputTels) {
             tel.addEventListener('keyup', function(event) {
                 if(event.key != 'Backspace' && (tel.value.length === 1 || tel.value.length === 5 || tel.value.length === 9 || tel.value.length === 12)) {
-                    tel.value += '-'
-                }
-            })
-        }
-    }
+                    tel.value += '-';
+                };
+            });
+        };
+    };
 
     addListenerDataClose() {
         document.addEventListener('click', function(event) {
             let target = event.target;
             if(target.dataset.close) {
                 ModalForm.close();
-                ModalCase.close()
+                ModalCase.close();
             };
         });
     };
 
     addListenerForModalForm() {
-        let record = document.getElementById('rootModal');
-        record.addEventListener('click', function() { // запись на бесплатное занятие через главную страницу
-            ModalForm.open(); // открытие модального окна
-            ModalForm.sendForm();
-            document.addEventListener('keydown', function(event) {
-                if(event.code.toLowerCase() == 'escape') {
-                    ModalForm.close();
-                };
+        let records = document.querySelectorAll('._order-form');
+        for( let record of records) {
+            record.addEventListener('click', function() { // запись на бесплатное занятие через главную страницу
+                ModalForm.open(); // открытие модального окна
+                ModalForm.sendForm();
+                document.addEventListener('keydown', function(event) {
+                    if(event.code.toLowerCase() == 'escape') {
+                        ModalForm.close();
+                    };
+                });
             });
-        });
+        };
     };
 
     addListenerForModalCase() {
-        let record = document.getElementById('rootModalCase');
+        let record = document.querySelector('._watch-case');
         record.addEventListener('click', function() { // запись на бесплатное занятие через главную страницу
             ModalCase.open(); // открытие модального окна
             document.addEventListener('keydown', function(event) {
@@ -60,6 +62,27 @@ class TechnicalFunctions {
             });
         });
     };
+
+    smoothNavigation() {
+        let menuLinks = document.querySelectorAll('._smooth-link[data-goto]');
+        if(menuLinks.length != 0) {
+            for(let menuLink of menuLinks) {
+                menuLink.addEventListener('click', (event) => {
+                    let target = event.target;
+                    if(target.dataset.goto && document.querySelector(target.dataset.goto)) {
+                        const gotoBlock = document.querySelector(target.dataset.goto);
+                        const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - 100;
+
+                        window.scrollTo({
+                            top: gotoBlockValue,
+                            behavior: 'smooth'
+                        });
+                        event.preventDefault();
+                    };
+                });            
+            };
+        };
+    };
 };
 
-export default new TechnicalFunctions()
+export default new TechnicalFunctions();
